728716023f1b1547219e111496f5e019ec12f7f9
Sowmyam22 <msowmyadeveloper@gmail.com>
2021-06-24 18:27:30 +0530

    adding the js code in the public folder and worked on the button onclikc for the delete button

---
+++ public/js/admin.js
@@ -0,0 +1,6 @@
+// javascript code that will not run the server, instead it will run on the client side
+
+const deleteProduct = (btn) => {
+    const productId = btn.parentNode.querySelector('[name=productId]').value;
+    const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+}
